c3c3c4dbdc043b9ad63621ad0d39698ece1f913f
Glitch (tidal-pet) <none>
2019-11-18 16:49:54 +0000

    🗻🏃 Checkpoint ./src/components/btnBar.js:692249/569

--- src/components/btnBar.js
+++ src/components/btnBar.js
@@ -4,17 +4,18 @@ import Button from "@material-ui/core/Button";
 import clsx from "clsx";
 
 const useStyles = makeStyles(theme => {
-  console.log(theme.palette.grey[500]);
+  console.log(theme);
   return {
     wrapper: {
       textAlign: "center"
     },
     button: {
-      cursor: "pointer",
+      cursor: 'pointer',
       margin: theme.spacing(0.5)
     },
     active: {
-      color: "red"
+      background: theme.palette.grey[200],
+      border: `1px solid ${theme.palette.grey[700]}`
     }
   };
 });
@@ -24,12 +25,12 @@ const BtnBar = ({ handleClick, activeLib, libraries }) => {
   return (
     <div className={classes.wrapper}>
       {libraries.map(lib => {
-        console.log(clsx(classes.button, { active: lib === activeLib }));
+        console.log(clsx(classes.root, { active: lib === activeLib }));
         return (
           <Button
             key={lib}
             variant="outlined"
-            className={clsx({ active: lib === activeLib }, classes.button)}
+            className={clsx(classes.button, { [classes.active]: lib === activeLib })}
           >
             {lib}
           </Button>
