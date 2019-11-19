e63c7767e2753d2ee917f488c4925775e02f7dce
Glitch (tidal-pet) <none>
2019-11-19 11:16:16 +0000

    🐕🏌 Checkpoint ./src/App.js:692249/1302

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
 import BtnBar from "./components/btnBar";
 import "./App.css";
 
@@ -13,17 +12,13 @@ const App = ({ data }) => {
 
   return (
     <main>
-      <BtnBar handleClick={handleLibClick} activeLib={activeLib} libraries={libs}/>
+      <BtnBar
+        handleClick={handleLibClick}
+        activeLib={activeLib}
+        libraries={libs}
+      />
     </main>
   );
-=======
-import Button from "@material-ui/core/Button";
-import "./App.css";
-
-const App = ({ data }) => {
-  const [libs, setLibs] = useState([...new Set(data.map(o => o.library))]);
-  return <Button>Hi</Button>;
->>>>>>> origin/master
 };
 
 export default App;
