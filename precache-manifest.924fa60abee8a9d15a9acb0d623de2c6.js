9f863a68dc5d9df59dee6de0344e5c1adaecde7c
Glitch (tidal-pet) <none>
2019-11-20 16:14:58 +0000

    Updates

---
+++ precache-manifest.924fa60abee8a9d15a9acb0d623de2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce3bd2d4c4181e35c5ece8cc3494f67",
+    "url": "/weekly-issues.git/index.html"
+  },
+  {
+    "revision": "a668431a6a27aeabb089",
+    "url": "/weekly-issues.git/static/css/main.9eb166b7.chunk.css"
+  },
+  {
+    "revision": "80330e3ad3fabb972843",
+    "url": "/weekly-issues.git/static/js/2.f55fae08.chunk.js"
+  },
+  {
+    "revision": "a668431a6a27aeabb089",
+    "url": "/weekly-issues.git/static/js/main.4f89f305.chunk.js"
+  },
+  {
+    "revision": "fae28a3376b29e7f1f59",
+    "url": "/weekly-issues.git/static/js/runtime-main.515b3d43.js"
+  },
+  {
+    "revision": "9de063ffd85456df5dd9c5244c9be931",
+    "url": "/weekly-issues.git/static/media/data.9de063ff.csv"
+  }
+]);
